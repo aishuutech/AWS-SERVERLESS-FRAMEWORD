54b14e26b3d4b1b542fbd7e252adf0490c12382a
Aiswarya K <aishuutech@gmail.com>
2022-11-18 19:27:34 +0530

    create user,getUserById,deleteById using serverless framework

---
+++ src/handlers.ts
@@ -0,0 +1,181 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { v4 } from "uuid";
+import * as yup from "yup";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "Coates";
+const headers = {
+  "content-type": "application/json",
+};
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().required(),
+  DOB: yup.number().required(),
+});
+
+export const createUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+    const user = {
+      ...reqBody,
+      id: v4(),
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: user,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(user),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+
+class HttpError extends Error {
+  constructor(public statusCode: number, body: Record<string, unknown> = {}) {
+    super(JSON.stringify(body));
+  }
+}
+
+const fetchUserById = async (id: string) => {
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        id: id,
+      },
+    })
+    .promise();
+
+  if (!output.Item) {
+    throw new HttpError(404, { error: "not found" });
+  }
+
+  return output.Item;
+};
+
+const handleError = (e: unknown) => {
+  if (e instanceof yup.ValidationError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        errors: e.errors,
+      }),
+    };
+  }
+
+  if (e instanceof SyntaxError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({ error: `invalid request body format : "${e.message}"` }),
+    };
+  }
+
+  if (e instanceof HttpError) {
+    return {
+      statusCode: e.statusCode,
+      headers,
+      body: e.message,
+    };
+  }
+
+  throw e;
+};
+
+export const getUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const product = await fetchUserById(event.pathParameters?.id as string);
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+
+export const updateUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event.pathParameters?.id as string;
+
+    await fetchUserById(id);
+
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+    const user = {
+      ...reqBody,
+      id: id,
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: user,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(user),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+
+export const deleteUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event.pathParameters?.id as string;
+
+    await fetchUserById(id);
+
+    await docClient
+      .delete({
+        TableName: tableName,
+        Key: {
+          id: id,
+        },
+      })
+      .promise();
+
+    return {
+      statusCode: 204,
+      body: "",
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+
+export const listUsers = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const output = await docClient
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(output.Items),
+  };
+};
